ef5efb75813c76bf6b0643ba638146f830429dc6
bengto <bengto@axis.com>
2024-12-06 07:45:11 +0100

    Day 6 part 1

--- aoc2024/Day6.cs
+++ aoc2024/Day6.cs
@@ -13,35 +13,99 @@ namespace aoc2024
         {
             var data = File.ReadAllLines(@"data\day6.txt");
 
-            List<int> cals = new List<int>();
+            var offsets = new[]{
+                new[]{-1, 0 }, // Up
+                new[]{0, 1}, // Eight
+                new[]{1, 0}, // Down
+                new[]{0, -1}, // Left
+            };
 
-            var values = data.Select(r => r.Split(' ')).ToArray();
+            var values = ArrayMethods.AddBorder(1, 'X', data).Select(vv => vv.ToArray()).ToArray();
 
-            /*
-            var values = data.Select(r => r.Length == 0 ? -1 : Int32.Parse(r)).ToArray();
+            int r = 0;
+            int c = 0;
+            int dir = 0;
 
-            var values = data.Select(r => r.Select(c => (int)(c - 'a')).ToArray()).ToArray();
+            for (r = 0; r < values.Length; r++)
+            {
+                c = Array.IndexOf(values[r], '^');
+                if (c > 0)
+                {
+                    break;
+                }
+            }
 
-            var values = data.Select(r => Int64.Parse(r)).ToArray();
-            */
+            while (values[r][c] != 'X')
+            {
+                values[r][c] = 'V'; // Visited
+                r += offsets[dir][0];
+                c += offsets[dir][1];
+
+                if (values[r][c] == '#')
+                {
+                    r -= offsets[dir][0];
+                    c -= offsets[dir][1];
+
+                    dir = (dir + 1) % 4;
+                }
+            }
 
 
+            Console.WriteLine($"Answer is {values.Sum(line => line.Count(cc => cc == 'V'))}");
+        }
 
-            Console.WriteLine($"Answer is {values.Count()}");
+        public bool WillLoop(Array<Array<char>> board)
+        {
+            int r = 0;
+            int c = 0;
+            int dir = 0;
+
+            for (r = 0; r < values.Length; r++)
+            {
+                c = Array.IndexOf(values[r], '^');
+                if (c > 0)
+                {
+                    break;
+                }
+            }
+
+            while (board[r][c] != 'X')
+            {
+                if (board[r][c] >= '0' && board[r][c] < '9')
+                {
+                    var marks = board[r][c] - '0';
+                }
+                values[r][c] = 'V'; // Visited
+                r += offsets[dir][0];
+                c += offsets[dir][1];
+
+                if (values[r][c] == '#')
+                {
+                    r -= offsets[dir][0];
+                    c -= offsets[dir][1];
+
+                    dir = (dir + 1) % 4;
+                }
+            }
+
         }
 
         public void Part2()
         {
             var data = File.ReadAllLines(@"data\day6.txt");
 
-            List<int> cals = new List<int>();
+            var offsets = new[]{
+                new[]{-1, 0 }, // Up
+                new[]{0, 1}, // Eight
+                new[]{1, 0}, // Down
+                new[]{0, -1}, // Left
+            };
 
-            var values = data.Select(r => r.Split(' ')).ToArray();
+            var values = ArrayMethods.AddBorder(1, 'X', data).Select(vv => vv.ToArray()).ToArray();
 
 
 
-
-            Console.WriteLine($"Answer is {values.Count()}");
+            Console.WriteLine($"Answer is {values.Sum(line => line.Count(cc => cc == 'V'))}");
         }
 
     }
