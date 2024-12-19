7bec67ab70b7e3d7685ca79af34efe8fed8ce5ef
Bengt Sirbelius <bengto@axis.com>
2024-12-19 11:57:30 +0100

    Day 19 done

--- aoc2024/Day19.cs
+++ aoc2024/Day19.cs
@@ -9,25 +9,95 @@ namespace aoc2024
 {
     internal class Day19
     {
+        string[] Towels;
+
+        internal bool StringMatch(string full, string part, int pos)
+        {
+            for (int p = 0; p < part.Length; p++)
+            {
+                if (pos+p >= full.Length || full[pos + p] != part[p])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool[] Tested;
+        Int64[] Result;
+
+        internal long IsPossible(string pattern, int pos)
+        {
+            if (pos == 0)
+            {
+                Tested = new bool[pattern.Length];
+                Result = new long[pattern.Length];
+            }
+
+            if (pos >= pattern.Length)
+            {
+                return 1;
+            }
+
+            if (Tested[pos])
+            {
+                return Result[pos];
+            }
+
+            var sum = Towels.Sum(s => StringMatch(pattern, s, pos) ? IsPossible(pattern, pos + s.Length) : 0);
+            Tested[pos] = true;
+            Result[pos] = sum;
+            return sum;
+        }
+
+        private static int LSort(string a, string b)
+        {
+            return a.Length - b.Length;
+        }
+
         public void Part1()
         {
             var data = File.ReadAllLines(@"data\day19.txt");
 
-            List<int> cals = new List<int>();
-
-            var values = data.Select(r => r.Split(' ')).ToArray();
-
             /*
-            var values = data.Select(r => r.Length == 0 ? -1 : Int32.Parse(r)).ToArray();
-
-            var values = data.Select(r => r.Select(c => (int)(c - 'a')).ToArray()).ToArray();
-
-            var values = data.Select(r => Int64.Parse(r)).ToArray();
+            data = new[]
+            {
+                "r, wr, b, g, bwu, rb, gb, br",
+                "",
+                "brwrr",
+                "bggr",
+                "gbbr",
+                "rrbgbr",
+                "ubwu",
+                "bwurrg",
+                "brgr",
+                "bbrgwb",
+            };
             */
 
+            Towels = data[0].Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
 
+            var patterns = data.Skip(2).ToArray();
 
-            Console.WriteLine($"Answer is {values.Count()}");
+            long sum = 0;
+            long part2sum = 0;
+
+            for (int i = 0; i < patterns.Length; i++)
+            {
+                Console.WriteLine($"String {i + 1} of {patterns.Length}");
+                var possible = IsPossible(patterns[i], 0);
+                if (possible > 0)
+                {
+                    sum += 1;
+                }
+                part2sum += possible;
+                Console.WriteLine($"Possible: {sum} ({possible} combos) of {i + 1}");
+            }
+
+            //sum = patterns.Sum(s => IsPossible(s, 0));
+
+            Console.WriteLine($"Answer is {sum}");
+            Console.WriteLine($"Answer for part 2 is {part2sum}");
         }
 
         public void Part2()
@@ -41,7 +111,7 @@ namespace aoc2024
 
 
 
-            Console.WriteLine($"Answer is {values.Count()}");
+            Console.WriteLine($"Answer is (see above)");
         }
 
     }
--- aoc2024/Program.cs
+++ aoc2024/Program.cs
@@ -10,9 +10,9 @@ namespace aoc2024
     {
         static void Main(string[] args)
         {
-            new Day18().Part1();
+            new Day19().Part1();
 
-            new Day18().Part2();
+            new Day19().Part2();
 
             Console.ReadLine();
         }
